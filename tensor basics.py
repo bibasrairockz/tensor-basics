a26d997aa5caaa5d322832eae177287c37770786
Bibas Rai <130794180+bibasrairockz@users.noreply.github.com>
2024-06-11 02:52:32 +0100

    Add files via upload

---
+++ tensor basics.py
@@ -0,0 +1,36 @@
+import torch
+import numpy as np
+
+x = torch.tensor([[1,1],
+               [2,2]])
+y = torch.tensor([[0,2],
+               [1,2]])
+print(x,"\n",y)
+z = torch.mm(x,y) #matrix
+z = torch.mul(x,y) # elementwise
+ 
+print(z)
+
+z = x * y
+print(z)
+
+x = np.ones(5)
+print(x)
+y =  torch.from_numpy(x)
+print(y)
+
+x = torch.rand(2,2)
+print(x.dtype)
+x = x.numpy()
+print(type(x))
+
+device = torch.device("cuda")
+
+x = torch.rand((2,2), device = device)
+print(x)
+y = np.ones((2,2))
+#print(x+y) # do not add as numpy does not work with cuda
+
+x = torch.rand(5, requires_grad= True)
+print(x)
+
